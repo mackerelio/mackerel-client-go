8fccc069489157d1fd43255867e1b09a1be160a4
y_uuki <yuki.tsubo@gmail.com>
2014-10-26 13:09:26 +0900

    Impl PostServiceMetricValues

--- metrics.go
+++ metrics.go
@@ -48,3 +48,31 @@ func (c *Client) PostHostMetricValues(metricValues [](*HostMetricValue)) error {
 
 	return nil
 }
+
+func (c *Client) PostServiceMetricValues(serviceName string, metricValues [](*ServiceMetricValue)) error {
+	requestJson, err := json.Marshal(metricValues)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(
+		"POST",
+		c.urlFor(fmt.Sprintf("/api/v0/services/%s/tsdb", serviceName)).String(),
+		bytes.NewReader(requestJson),
+	)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := c.Request(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 200 {
+		return errors.New(fmt.Sprintf("API result failed: %s", resp.Status))
+	}
+
+	return nil
+}
--- metrics_test.go
+++ metrics_test.go
@@ -69,3 +69,59 @@ func TestPostHostMetricValues(t *testing.T) {
 		t.Error("err shoud be nil but: ", err)
 	}
 }
+
+func TestPostServiceMetricValues(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/api/v0/services/My-Service/tsdb" {
+			t.Error("request URL should be /api/v0/services/My-Service/tsdb but :", req.URL.Path)
+		}
+
+		if req.Method != "POST" {
+			t.Error("request method should be POST but: ", req.Method)
+		}
+
+		body, _ := ioutil.ReadAll(req.Body)
+
+		var values []struct {
+			Name  string      `json:"name"`
+			Time  float64     `json:"time"`
+			Value interface{} `json:"value"`
+		}
+
+		err := json.Unmarshal(body, &values)
+		if err != nil {
+			t.Fatal("request body should be decoded as json", string(body))
+		}
+
+		if values[0].Name != "proxy.access_log.latency" {
+			t.Error("request sends json including name but: ", values[0].Name)
+		}
+		if values[0].Time != 123456789 {
+			t.Error("request sends json including time but: ", values[0].Time)
+		}
+		if values[0].Value.(float64) != 500 {
+			t.Error("request sends json including value but: ", values[0].Value)
+		}
+
+		respJson, _ := json.Marshal(map[string]bool{
+			"success": true,
+		})
+
+		res.Header()["Content-Type"] = []string{"application/json"}
+		fmt.Fprint(res, string(respJson))
+	}))
+	defer ts.Close()
+
+	client, _ := NewClientForTest("dummy-key", ts.URL, false)
+	err := client.PostServiceMetricValues("My-Service", []*ServiceMetricValue{
+		&ServiceMetricValue{
+			Name:  "proxy.access_log.latency",
+			Time:  123456789,
+			Value: 500,
+		},
+	})
+
+	if err != nil {
+		t.Error("err shoud be nil but: ", err)
+	}
+}
