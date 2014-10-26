482a538ae8e2bbc1092a4e2bf7ffb09f9f8d1f24
y_uuki <yuki.tsubo@gmail.com>
2014-10-26 12:45:27 +0900

    Impl PostHostMetricValues

---
+++ metrics.go
@@ -0,0 +1,50 @@
+package mackerel
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type HostMetricValue struct {
+	HostId string      `json:"hostId",omitempty`
+	Name   string      `json:"name,omitempty"`
+	Time   float64     `json:"time,omitempty"`
+	Value  interface{} `json:"value,omitempty"`
+}
+
+type ServiceMetricValue struct {
+	Name  string      `json:"name,omitempty"`
+	Time  float64     `json:"time,omitempty"`
+	Value interface{} `json:"value,omitempty"`
+}
+
+func (c *Client) PostHostMetricValues(metricValues [](*HostMetricValue)) error {
+	requestJson, err := json.Marshal(metricValues)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(
+		"POST",
+		c.urlFor("/api/v0/tsdb").String(),
+		bytes.NewReader(requestJson),
+	)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := c.Request(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 200 {
+		return errors.New(fmt.Sprintf("API result failed: %s", resp.Status))
+	}
+
+	return nil
+}
---
+++ metrics_test.go
@@ -0,0 +1,71 @@
+package mackerel
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPostHostMetricValues(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/api/v0/tsdb" {
+			t.Error("request URL should be /api/v0/tsdb but :", req.URL.Path)
+		}
+
+		if req.Method != "POST" {
+			t.Error("request method should be POST but: ", req.Method)
+		}
+
+		body, _ := ioutil.ReadAll(req.Body)
+
+		var values []struct {
+			HostId string      `json:"hostId"`
+			Name   string      `json:"name"`
+			Time   float64     `json:"time"`
+			Value  interface{} `json:"value"`
+		}
+
+		err := json.Unmarshal(body, &values)
+		if err != nil {
+			t.Fatal("request body should be decoded as json", string(body))
+		}
+
+		if values[0].HostId != "9rxGOHfVF8F" {
+			t.Error("request sends json including hostId but: ", values[0].HostId)
+		}
+		if values[0].Name != "custom.metric.mysql.connections" {
+			t.Error("request sends json including name but: ", values[0].Name)
+		}
+		if values[0].Time != 123456789 {
+			t.Error("request sends json including time but: ", values[0].Time)
+		}
+		if values[0].Value.(float64) != 100 {
+			t.Error("request sends json including value but: ", values[0].Value)
+		}
+
+		respJson, _ := json.Marshal(map[string]bool{
+			"success": true,
+		})
+
+		res.Header()["Content-Type"] = []string{"application/json"}
+		fmt.Fprint(res, string(respJson))
+	}))
+	defer ts.Close()
+
+	client, _ := NewClientForTest("dummy-key", ts.URL, false)
+	err := client.PostHostMetricValues([]*HostMetricValue{
+		&HostMetricValue{
+			HostId: "9rxGOHfVF8F",
+			Name:   "custom.metric.mysql.connections",
+			Time:   123456789,
+			Value:  100,
+		},
+	})
+
+	if err != nil {
+		t.Error("err shoud be nil but: ", err)
+	}
+}
