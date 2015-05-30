a864ff68a2b9e1287bb38766cbecf60ffea04ff7
Songmu <y.songmu@gmail.com>
2015-05-31 04:21:04 +0900

    add graph_defs_test.go

---
+++ graph_defs_test.go
@@ -0,0 +1,75 @@
+package mackerel
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCreateGraphDefs(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/api/v0/graph-defs/create" {
+			t.Error("request URL should be /api/v0/graph-defs/create but :", req.URL.Path)
+		}
+
+		if req.Method != "POST" {
+			t.Error("request method should be GET but :", req.Method)
+		}
+		body, _ := ioutil.ReadAll(req.Body)
+
+		var datas []struct {
+			Name        string            `json:"name"`
+			DisplayName string            `json:"displayName"`
+			Unit        string            `json:"unit"`
+			Metrics     []GraphDefsMetric `json:"metrics"`
+		}
+
+		err := json.Unmarshal(body, &datas)
+		if err != nil {
+			t.Fatal("request body should be decoded as json", string(body))
+		}
+		data := datas[0]
+
+		if data.Name != "mackerel" {
+			t.Error("request sends json including name but: %s", data.Name)
+		}
+		if data.DisplayName != "HorseMackerel" {
+			t.Error("request sends json including DisplayName but: %s", data.Name)
+		}
+
+		respJSON, _ := json.Marshal(map[string]string{
+			"result": "OK",
+		})
+		res.Header()["Content-Type"] = []string{"application/json"}
+		fmt.Fprint(res, string(respJSON))
+	}))
+	defer ts.Close()
+
+	client, _ := NewClientWithOptions("dummy-key", ts.URL, false)
+	err := client.CreateGraphDefs([]GraphDefsParam{
+		GraphDefsParam{
+			Name:        "mackerel",
+			DisplayName: "HorseMackerel",
+			Unit:        "percentage",
+			Metrics: []GraphDefsMetric{
+				GraphDefsMetric{
+					Name:        "saba1",
+					DisplayName: "aji1",
+					IsStacked:   false,
+				},
+				GraphDefsMetric{
+					Name:        "saba2",
+					DisplayName: "aji2",
+					IsStacked:   false,
+				},
+			},
+		},
+	})
+
+	if err != nil {
+		t.Error("err shoud be nil but: ", err)
+	}
+}
