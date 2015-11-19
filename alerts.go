03dc5f35feee83e2430babed302e32ce3b52ec93
Shinji Tanaka <shinji.tanaka@gmail.com>
2015-11-19 19:34:22 +0900

    support alerts api

---
+++ alerts.go
@@ -0,0 +1,86 @@
+package mackerel
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+/*
+{
+  "alerts": [
+    {
+      "id": "2wpLU5fBXbG",
+      "status": "CRITICAL",
+      "monitorId": "2cYjfibBkaj",
+      "type": "connectivity",
+      "openedAt": 1445399342,
+      "hostId": "2vJ965ygiXf"
+    },
+    {
+      "id": "2ust8jNxFH3",
+      "status": "CRITICAL",
+      "monitorId": "2cYjfibBkaj",
+      "type": "connectivity",
+      "openedAt": 1441939801,
+      "hostId": "2tFrtykgMib"
+    }
+  ]
+}
+*/
+
+// Alert information
+type Alert struct {
+	ID        string `json:"id,omitempty"`
+	Status    string `json:"status,omitempty"`
+	MonitorID string `json:"monitorId,omitempty"`
+	Type      string `json:"type,omitempty"`
+	HostID    string `json:"hostId,omitempty"`
+	Value     uint64 `json:"value,omitempty"`
+	Message   string `json:"message,omitempty"`
+	Reason    string `json:"reason,omitempty"`
+	OpenedAt  uint64 `json:"openedAt,omitempty"`
+	ClosedAt  uint64 `json:"closedAt,omitempty"`
+}
+
+// FindAlerts find monitors
+func (c *Client) FindAlerts() ([]*Alert, error) {
+	req, err := http.NewRequest("GET", c.urlFor("/api/v0/alerts").String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.Request(req)
+	defer closeResponse(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	var data struct {
+		Alerts []*(Alert) `json:"alerts"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+	return data.Alerts, err
+}
+
+// CloseAlert update monitor
+func (c *Client) CloseAlert(alertID string, reason string) (*Alert, error) {
+	var reqBody struct {
+		Reason string `json:"reason"`
+	}
+	reqBody.Reason = reason
+	resp, err := c.PostJSON(fmt.Sprintf("/api/v0/alerts/%s/close", alertID), &reqBody)
+	defer closeResponse(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	var data Alert
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
