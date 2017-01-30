59ecae998627b8fa2dd1c95da1f52d87b8ed695f
Yasuhisa Yoshida <syou6162@gmail.com>
2017-01-30 16:44:57 +0900

    sort fields

--- graph_annotation.go
+++ graph_annotation.go
@@ -2,12 +2,12 @@ package mackerel
 
 // GraphAnnotation represents parameters to post graph annotation.
 type GraphAnnotation struct {
-	Service     string   `json:"service,omitempty"`
-	Roles       []string `json:"roles,omitempty"`
-	From        int64    `json:"from,omitempty"`
-	To          int64    `json:"to,omitempty"`
 	Title       string   `json:"title,omitempty"`
 	Description string   `json:"description,omitempty"`
+	From        int64    `json:"from,omitempty"`
+	To          int64    `json:"to,omitempty"`
+	Service     string   `json:"service,omitempty"`
+	Roles       []string `json:"roles,omitempty"`
 }
 
 // CreateGraphAnnotation creates graph annotation.
