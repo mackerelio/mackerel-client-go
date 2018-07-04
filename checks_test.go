8c4868e375cda4ef8c7b01bd8d2a30900910f153
Songmu <y.songmu@gmail.com>
2018-07-04 18:15:09 +0900

    TestClient_ReportCheckMonitors

--- checks_test.go
+++ checks_test.go
@@ -2,6 +2,10 @@ package mackerel
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -24,4 +28,75 @@ func TestCheckReports_MarshalJSON(t *testing.T) {
 	if got != expect {
 		t.Errorf("expect: %s, but: %s", expect, got)
 	}
+}
+
+func TestClient_ReportCheckMonitors(t *testing.T) {
+	crs := &CheckReports{
+		Reports: []*CheckReport{
+			{
+				Source:     NewCheckSourceHost("hogee"),
+				Name:       "chchch",
+				Status:     CheckStatusCritical,
+				OccurredAt: 100,
+				Message:    "OKOK",
+			},
+		},
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		reqPath := "/api/v0/monitoring/checks/report"
+		if req.URL.Path != reqPath {
+			t.Errorf("request URL should be %s but: %s", reqPath, req.URL.Path)
+		}
+
+		if req.Method != "POST" {
+			t.Error("request method should be POST but: ", req.Method)
+		}
+
+		body, _ := ioutil.ReadAll(req.Body)
+
+		var values struct {
+			Reports []struct {
+				Source     interface{} `json:"source"`
+				Name       string      `json:"name"`
+				Status     string      `json:"status"`
+				Message    string      `json:"message"`
+				OccurredAt int64       `json:"occurredAt"`
+			} `json:"reports"`
+		}
+
+		err := json.Unmarshal(body, &values)
+		if err != nil {
+			t.Fatal("request body should be decoded as json", string(body))
+		}
+
+		r := values.Reports[0]
+		if r.Name != "chchch" {
+			t.Error("request sends json including hostId but: ", r.Name)
+		}
+		if r.OccurredAt != 100 {
+			t.Error("request sends json including time but: ", r.OccurredAt)
+		}
+		if r.Status != CheckStatusCritical {
+			t.Error("request sends json including value but: ", r.Status)
+		}
+		if r.Message != "OKOK" {
+			t.Error("request sends json including value but: ", r.Message)
+		}
+
+		respJSON, _ := json.Marshal(map[string]bool{
+			"success": true,
+		})
+		res.Header()["Content-Type"] = []string{"application/json"}
+		fmt.Fprint(res, string(respJSON))
+	}))
+	defer ts.Close()
+
+	cli, _ := NewClientWithOptions("dummy-key", ts.URL, false)
+	err := cli.ReportCheckMonitors(crs)
+
+	if err != nil {
+		t.Error("err should be nil but: ", err)
+	}
+
 }
