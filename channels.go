f0b2c7440092303cc463dc0c887f6fb83b0ffb37
Stefan Utamaru <stefafafan@hatena.ne.jp>
2020-01-09 22:11:38 +0900

    getting ready to implement the channels and channels_test.go files

---
+++ channels.go
@@ -0,0 +1,3 @@
+package mackerel
+
+// TODO
---
+++ channels_test.go
@@ -0,0 +1,11 @@
+package mackerel
+
+// TODO: Write test and implementation for each of the following.
+
+// Getting Notification Channels
+//   GET /api/v0/channels
+// Registering Notification Channels
+//  POST /api/v0/channels
+// Deleting Notification Channels
+//  DELETE /api/v0/channels/<channelId>
+// https://mackerel.io/api-docs/entry/channels
