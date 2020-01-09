45d57eb17c79cb1ccfbb3344b737a591c7a8defe
Stefan Utamaru <stefafafan@hatena.ne.jp>
2020-01-09 22:43:44 +0900

    define channel struct

--- channels.go
+++ channels.go
@@ -1,3 +1,28 @@
 package mackerel
 
 // TODO
+
+// Channel represents a Mackerel notification channel.
+type Channel struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+
+	// Exists when the type is "email"
+	Emails  []string `json:"emails,omitempty"`
+	UserIDs []string `json:"userIds,omitempty"`
+
+	// Exists when the type is "slack"
+	Mentions struct {
+		OK       string `json:"ok,omitempty"`
+		Warning  string `json:"warning,omitempty"`
+		Critical string `json:"critical,omitempty"`
+	} `json:"mentions,omitempty"`
+	EnabledGraphImage bool `json:"enabledGraphImage,omitempty"`
+
+	// Exists when the type is "slack" or "webhook"
+	URL string `json:"url,omitempty"`
+
+	// Exists when the type is "email", "slack", or "webhook"
+	Events []string `json:"events,omitempty"`
+}
