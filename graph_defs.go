ba4b85ac4dd328271e84091b5d83046197782c25
lufia <lufia@lufia.org>
2020-01-24 11:06:12 +0900

    add omitempty to types for using to create graph-defs

--- graph_defs.go
+++ graph_defs.go
@@ -3,15 +3,15 @@ package mackerel
 // GraphDefsParam parameters for posting graph definitions
 type GraphDefsParam struct {
 	Name        string             `json:"name"`
-	DisplayName string             `json:"displayName"`
-	Unit        string             `json:"unit"`
+	DisplayName string             `json:"displayName,omitempty"`
+	Unit        string             `json:"unit,omitempty"`
 	Metrics     []*GraphDefsMetric `json:"metrics"`
 }
 
 // GraphDefsMetric graph metric
 type GraphDefsMetric struct {
 	Name        string `json:"name"`
-	DisplayName string `json:"displayName"`
+	DisplayName string `json:"displayName,omitempty"`
 	IsStacked   bool   `json:"isStacked"`
 }
 
