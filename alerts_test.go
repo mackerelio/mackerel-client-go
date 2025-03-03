08edb4a5de4b6ba94a5c24f865ec8950789a5d46
appare45 <appare45@hatena.com>
2025-03-03 10:50:12 +0900

    Implemented the alerts logs API

--- alerts_test.go
+++ alerts_test.go
@@ -381,3 +381,104 @@ func TestGetAlert(t *testing.T) {
 		t.Errorf("Wrong data for alert: %v", alert)
 	}
 }
+
+func TestFindAlertLogs(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		url := fmt.Sprintf("/api/v0/alerts/%s/logs", "2wpLU5fBXbG")
+		if req.URL.Path != url {
+			t.Error("request URL should be /api/v0/alerts/<ID>/logs but: ", req.URL.Path)
+		}
+
+		if req.Method != "GET" {
+			t.Error("request method should be GET but: ", req.Method)
+		}
+
+		respJSON, _ := json.Marshal(map[string]interface{}{
+			"logs": []map[string]interface{}{
+				{
+					"id":          "5m7fewuu5tS",
+					"createdAt":   1735290407,
+					"status":      "WARNING",
+					"trigger":     "monitoring",
+					"monitorId":   "5m72DB7s7sU",
+					"targetValue": (*float64)(nil),
+					"statusDetail": map[string]interface{}{
+						"type": "check",
+						"detail": map[string]interface{}{
+							"message": "Uptime WARNING: 0 day(s) 0 hour(s) 6 minute(s) (398 second(s))",
+							"memo":    "",
+						},
+					},
+				}, {
+					"id":           "5m7fewuu5tS",
+					"createdAt":    1735290407,
+					"status":       "WARNING",
+					"trigger":      "monitoring",
+					"monitorId":    "5m72DB7s7sU",
+					"targetValue":  (*float64)(nil),
+					"statusDetail": nil,
+				},
+			},
+			"nextId": "2fsf8jRxFG1",
+		})
+
+		res.Header()["Content-Type"] = []string{"application/json"}
+		fmt.Fprint(res, string(respJSON))
+	}))
+	defer ts.Close()
+
+	client, _ := NewClientWithOptions("dummy-key", ts.URL, false)
+	logs, err := client.FindAlertLogs("2wpLU5fBXbG")
+	if err != nil {
+		t.Error("err should be nil but: ", err)
+	}
+
+	if len(logs.AlertLogs) != 2 {
+		t.Error("logs should have 1 elements but: ", len(logs.AlertLogs))
+	}
+
+	if logs.NextID != "2fsf8jRxFG1" {
+		t.Error("request sends json including nextId but: ", logs.NextID)
+	}
+
+	if logs.AlertLogs[0].ID != "5m7fewuu5tS" {
+		t.Error("alert id should be \"5m7fewuu5tS\" but: ", logs.AlertLogs[0].ID)
+	}
+
+	if logs.AlertLogs[0].CreatedAt != 1735290407 {
+		t.Error("createdAt should be 1735290407 but: ", logs.AlertLogs[0].CreatedAt)
+	}
+
+	if logs.AlertLogs[0].Trigger != "monitoring" {
+		t.Error("trigger should be \"monitoring\" but: ", logs.AlertLogs[0].Trigger)
+	}
+
+	if *logs.AlertLogs[0].MonitorID != "5m72DB7s7sU" {
+		t.Error("monitorId should be \"5m72DB7s7sU\" but: ", *logs.AlertLogs[0].MonitorID)
+	}
+
+	if logs.AlertLogs[0].TargetValue != nil {
+		t.Error("targetValue should be nil but: ", logs.AlertLogs[0].TargetValue)
+	}
+
+	if logs.AlertLogs[0].Status != "WARNING" {
+		t.Error("alert status should be \"WARNING\" but: ", logs.AlertLogs[0].Status)
+	}
+
+	if logs.AlertLogs[0].StatusDetail.Type != "check" {
+		t.Error("statusDetail type should be \"check\" but: ", logs.AlertLogs[0].StatusDetail.Type)
+	}
+
+	if logs.AlertLogs[0].StatusDetail.Detail.Message != "Uptime WARNING: 0 day(s) 0 hour(s) 6 minute(s) (398 second(s))" {
+		t.Error("statusDetail message should be \"Uptime WARNING: 0 day(s) 0 hour(s) 6 minute(s) (398 second(s))\" but: ", logs.AlertLogs[0].StatusDetail.Detail.Message)
+	}
+
+	if logs.AlertLogs[0].StatusDetail.Detail.Memo != "" {
+		t.Error("statusDetail memo should be empty but: ", logs.AlertLogs[0].StatusDetail.Detail.Memo)
+	}
+
+	if logs.AlertLogs[1].StatusDetail != nil {
+		t.Error("statusDetail should be nil but: ", logs.AlertLogs[1].StatusDetail)
+	}
+
+}
